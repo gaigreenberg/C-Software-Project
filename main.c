89d93b63d7a848a0d259ea8a04fee42d334116a6
omlad93 <58659912+omlad93@users.noreply.github.com>
2020-08-26 14:43:09 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 /* calculate M given matrix A - A is a spmat  */
 #include "spmat.h"
 #include "Bmat.h"
+#include "division.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stddef.h>
@@ -22,7 +23,7 @@ int calculateM(spmat* A ,int n ){
 
 }
 
-void updateRow(int* row, int n, int* mask, int k){
+void updateRow(double* row, int n, int* mask, int k){
 	int i, neighbor, j=0;
 
 	if (k == 0){
@@ -48,16 +49,18 @@ void updateRow(int* row, int n, int* mask, int k){
 /* read input and store in matrix, after allocation*/
 void loadMatrix(FILE* input, spmat* matrix, int n){
 	int i,r,k;
-	int *row, *mask;
+	int *mask;
+	double *row;
 
 	rewind(input);
 	r = fread(&n, 1, sizeof(int), input);
 	assert(r==1);
-	row =  (int*)calloc(n, sizeof(int));
+	row =  (double*)calloc(n, sizeof(int));
 	mask = (int*)calloc(n, sizeof(int));
 	for (i=0; i<n; i++){
 		fread(&k, 1 ,sizeof(int) , input);
 		matrix->Kvec[i]=k;
+		fread(&row, n , sizeof(int), input);
 		updateRow(row,n,mask,k);
 		AddRow(matrix , row, i);
 	}
@@ -66,51 +69,64 @@ void loadMatrix(FILE* input, spmat* matrix, int n){
 }
 
 /* write partiton with maximized modularity*/
-void writePartition(FILE* output, int n){
+void wrtieDivision(FILE* output, division* div){
+	int j, k, n=div->size;
+	int* members;
+	GroupCell* cell = div->groups;
 
+	fwrite(&n, sizeof(int), 1 , output);
+
+	for (j=0;j<n;j++){
+		k = groupSize(cell);
+		members = groupRep(cell);
+		fwrite(&k, sizeof(int), 1, output);
+		fwrite(members, sizeof(int), k, output);
+		free(members);
+
+	}
 }
 
-/*B should be created before, Bg should be allocated before*/
-int Alogrithem2(spmat* B, spmat* Bg, int* g, int n){
-	double *value, *vec;
-	int j,*s,Q;
-	vec = (double*)calloc(n, sizeof(double));
-	s   = (int*)calloc(n,sizeof(int));
+/*B should be created before, Bg,subDivision should be allocated before*/
+int Alogrithem2(spmat* B, spmat* Bg, int* subDivision, int* g, int n, int* a, int* b){
+	double value, *vec, Q;
+	int j;
+
+	*a=0 ; *b=0;
+	vec 		  = (double*)calloc(n, sizeof(double));
+	subDivision   = (int*)calloc(n,sizeof(int));
 	createBg(B,g,Bg);
 	createBgHat(Bg);
 
-	value = calculateEigenPair(vec, value, BgHat);
+	value = calculateEigenPair(vec, Bg, n);
 
 	if (value <=0){
 		return 0;
 	}
+
 	for(j=0; j<n ; j++){
-		if(vec[j]<=0){
-			s[j] = -1;
-		}else{
-			s[j] = 1;
+		if(vec[j]<0){
+			subDivision[j] = -1;
+			++(*a);
+		}else if(vec[j]>0){
+			subDivision[j] = 1;
+			++(*b);
 		}
 
 	}
-	Q = calculateQ(s, B);
+	Q = calculateQ(subDivision, B);
 	if(Q < 0){
 		return 0;
 	}
-	for (j=0; j<n; j++){
-		g[j] = g[j] * s[j];
-	}
 
 	free(vec);
-	free(s);
 	return 1;
 }
-
-
 
 /* argv[1] = input || argv[2] = output */
 int main(int argc, char* argv[]) {
 	FILE *inMatrix, *outMatrix;
-	spmat *A , *B, *C;
+	spmat *A;
+	division* div=calloc(1,sizeof(division));
 	int n;
 
 	if(argc != 3){
@@ -120,14 +136,14 @@ int main(int argc, char* argv[]) {
 	inMatrix  = fopen(argv[1],"r");
 	outMatrix = fopen(argv[2],"w");
 	fread(&n, 1 ,sizeof(int), inMatrix);
-	A = allocateMatrix(n); B = allocateMatrix(n) ; C = allocateMatrix(n);
+	A = allocateMatrix(n);/* B = allocateMatrix(n) ; C = allocateMatrix(n);*/
 	loadMatrix(inMatrix, A, n);
 
 
 	/* BODY */
 
 
-	writePartition(outMatrix, n);
+	wrtieDivision(outMatrix,div);
 
 	return 0;
 }
